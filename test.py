fe74bcb48b55276752ebd8db71fb2f0c9aa20d57
hoangngoclam298 <hoangngoclammal1@gmail.com>
2023-05-25 10:29:27 +0700

    fix data merge for my data

---
+++ test.py
@@ -0,0 +1,11 @@
+import random
+import os
+import csv
+import pandas as pd
+
+
+df = pd.read_csv(r'C:\Users\LAMHN\Documents\Mobifone\small_dataset\small_dataset\train1\list.csv', delimiter=",", header=None)
+
+print(len(df))
+
+
